e03118a5ae3da58332fd5b61113cef0a4db1596c
V_Kovshov <ssevenn41@gmail.com>
2024-01-10 22:17:57 +0200

    added some changes

--- finder_email_verifier.py
+++ finder_email_verifier.py
@@ -1,138 +1,154 @@
-"""Module that checks email addresses."""
+"""Client for searching and verifying emails."""
+
 import requests
 
+API_KEY = 'e3072867b4808be5e694697d2ffa683e026d9ecb'
+URL_VERIFY = 'https://api.hunter.io/v2/email-verifier'
+URL_DOMAIN_SEARCH = 'https://api.hunter.io/v2/domain-search'
 
-class EmailVerifier(object):
-    """The class performs email verification and validation."""
-
-    def __init__(self, api_key: str) -> None:
-        """Initialize the EmailVerifier class.
-
-        :param api_key: api key of the hunter io.
-        """
-        self.api_key = api_key
-        self.url_verify = 'https://api.hunter.io/v2/email-verifier'
-        self.database_verify = {}
-
-    def check_valid_email(self, email: str) -> bool | str:
-        """Do email verification and validation.
-
-        :param email: validation email
-        :return: bool
-        """
-        params_api = {'api_key': self.api_key, 'email': email}
-        try:
-            data_api = requests.get(url=self.url_verify, params=params_api, timeout=10).json()
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return 'Ops...something went wrong...'
-        return data_api['data']['status'] == 'valid'
-
-    def create(self, email: str) -> None | str:
-        """Add a new e-mail entry to the dictionary.
-
-        :param email: validation email
-        :return: None or str
-        """
-        if email not in self.database_verify:
-            check_email = self.check_valid_email(email)
-            self.database_verify[email] = 'valid' if check_email else 'invalid'
-        else:
-            return 'Email there is in database'
-
-    def read(self) -> dict:
-        """Let's read the dictionary with the entries.
-
-        :return: dict
-        """
-        return self.database_verify
-
-    def update(self, email: str) -> None | str:
-        """Let's update the e-mail data in the dictionary.
-
-        :param email: validation email
-        :return: None or str
-        """
-        if email not in self.database_verify:
-            return 'Ops...email does not exist in database.'
-        else:
-            check_email = self.check_valid_email(email)
-            self.database_verify[email] = 'valid' if check_email else 'invalid'
-
-    def delete(self, email: str) -> None | str:
-        """Let's remove the e-mail data from the dictionary.
-
-        :param email: validation email
-        :return: None or str
-        """
-        if email in self.database_verify:
-            self.database_verify.pop(email)
-        else:
-            return 'Email does not exist in database.'
+verify_emails = {}
+emails_by_domain = {}
 
 
-class DomainSearch(object):
-    """The class searches for email addresses by a given domain."""
+def verify_email(email: str) -> str:
+    """Do email verification and validation.
 
-    def __init__(self, api_key):
-        """Initialize the DomainSearch class.
+    :param email: validation email
+    :return: str
+    """
+    params_api = {'api_key': API_KEY, 'email': email}
+    try:
+        response = requests.get(url=URL_VERIFY, params=params_api, timeout=10)
+    except requests.RequestException:
+        raise requests.RequestException()
+    response.raise_for_status()
+    data_response = response.json()
+    return data_response['data']['status']
 
-        :param api_key: api key of the hunter io.
-        """
-        self.api_key = api_key
-        self.url = 'https://api.hunter.io/v2/domain-search'
-        self.database = {}
 
-    def search_emails(self, domain: str) -> str | dict:
-        """Find emails by domain name.
+def create_verify_email(email: str) -> None:
+    """Add a new e-mail entry to the dictionary.
 
-        :param domain: email address lookup domain
-        :return: None or dict of result
-        """
-        params_search = {'domain': domain, 'api_key': self.api_key}
-        try:
-            data_search = requests.get(self.url, params=params_search, timeout=10).json()
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return 'Ops...something went wrong...'
-        return data_search['data']['emails']
+    :param email: validation email
+    :return: None
+    """
+    if email not in verify_emails:
+        status_email = verify_email(email)
+        verify_emails[email] = status_email
+    else:
+        raise KeyError(email, 'already exists in the database')
 
-    def create(self, domain: str) -> None | str:
-        """Let's add the domain with the list of emails to the database.
 
-        :param domain: email address lookup domain
-        :return: None or str
-        """
-        if domain not in self.database:
-            emails = self.search_emails(domain)
-            self.database[domain] = [email['value'] for email in emails]
-        else:
-            return 'Domain already exists in the database.'
+def read_verify_email(email: str = None) -> dict | str:
+    """Let's read the dictionary with the entries.
 
-    def read(self) -> dict:
-        """Let's read the data from the database.
+    :return: dict or str
+    """
+    if email is None:
+        return verify_emails
+    return verify_emails.get(email)
 
-        :return: data dictionary
-        """
-        return self.database
 
-    def update(self, domain: str) -> None | str:
-        """Update the email address of the domain in our database.
+def update_verify_email(email: str) -> None:
+    """Let's update the e-mail data in the dictionary.
 
-        :param domain: domain name in the database
-        :return: None or str
-        """
-        if domain in self.database:
-            emails = self.search_emails(domain)
-            self.database[domain] = [email['value'] for email in emails]
-        else:
-            return 'The domain does not exist in the database. You need to create it first.'
+    :param email: validation email
+    :return: None
+    """
+    if email not in verify_emails:
+        raise KeyError('No result found for', email)
+    else:
+        check_email = verify_email(email)
+        verify_emails[email] = check_email
 
-    def delete(self, domain: str) -> None | str:
-        """Delete domain email addresses in our database.
 
-        :param domain: domain name in the database
-        :return: None or str
-        """
-        if domain in self.database:
-            self.database.pop(domain)
-        else:
-            return 'The domain does not exist in the database.'
+def delete_verify_email(email: str) -> None:
+    """Let's remove the e-mail data from the dictionary.
+
+    :param email: validation email
+    :return: None
+    """
+    if email in verify_emails:
+        verify_emails.pop(email)
+    else:
+        raise KeyError('No result found for', email)
+
+
+def search_emails_by_domain(domain: str) -> str:
+    """Find emails by domain name.
+
+    :param domain: email address lookup domain
+    :return: str
+    """
+    params_search = {'domain': domain, 'api_key': API_KEY}
+    try:
+        response = requests.get(URL_DOMAIN_SEARCH, params=params_search, timeout=10)
+    except requests.RequestException:
+        raise requests.RequestException()
+    response.raise_for_status()
+    data_response = response.json()
+    return data_response['data']['emails']
+
+
+def create_emails_by_domain(domain: str) -> None:
+    """Let's add the domain with the list of emails to the database.
+
+    :param domain: email address lookup domain
+    :return: None
+    """
+    if domain not in emails_by_domain:
+        emails = search_emails_by_domain(domain)
+        emails_by_domain[domain] = [email['value'] for email in emails]
+    else:
+        raise KeyError(domain, 'already exists in the database')
+
+
+def read_emails_by_domain(domain: str = None) -> dict:
+    """Let's read the data from the database.
+
+    :return: data dictionary
+    """
+    if domain is None:
+        return emails_by_domain
+    return emails_by_domain.get(domain)
+
+
+def update_emails_by_domain(domain: str) -> None:
+    """Update the email address of the domain in our database.
+
+    :param domain: domain name in the database
+    :return: None
+    """
+    if domain in emails_by_domain:
+        emails = search_emails_by_domain(domain)
+        emails_by_domain[domain] = [email['value'] for email in emails]
+    else:
+        raise KeyError('No result found for', domain)
+
+
+def delete_emails_by_domain(domain: str) -> None | str:
+    """Delete domain email addresses in our database.
+
+    :param domain: domain name in the database
+    :return: None or str
+    """
+    if domain in emails_by_domain:
+        emails_by_domain.pop(domain)
+    else:
+        return 'The domain does not exist in the database.'
+
+
+if __name__ == '__main__':
+    create_verify_email('patrick@stripe.com')
+    print(read_verify_email())
+    update_verify_email('patrick@stripe.com')
+    print(read_verify_email())
+    delete_verify_email('patrick@stripe.com')
+    print(read_verify_email())
+
+    create_emails_by_domain('google.com')
+    create_emails_by_domain('hunter.io')
+    print(read_emails_by_domain())
+    delete_emails_by_domain('google.com')
+    print(read_emails_by_domain())
+    update_emails_by_domain('github.com')
